18a34fb4fa497c3c89f4c0dc3acb36d1f1308e94
TZbooo <helloworldbooo@gmail.com>
2023-12-01 11:52:43 +0300

    fix

--- cars_uploader.py
+++ cars_uploader.py
@@ -32,7 +32,7 @@ car_list = []
 with open('table.csv', 'r') as csv_file:
     table = csv.reader(csv_file)
 
-    for row in list(table)[1584:]:
+    for row in list(table)[2175:]:
         car_list.append(Car(
             id=row[0],
             title=row[1],
